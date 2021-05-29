19a9ce2e497d50d1d3974154c4f4f15ea7e435ed
Sebastian Miklaszewski <sebastian.miklaszewski@valueadd.pl>
2021-05-29 19:41:02 +0200

    init commit

---
+++ src/test/java/semi/example/spring_bouncer/SpringBouncerApplicationTests.java
@@ -0,0 +1,13 @@
+package semi.example.spring_bouncer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBouncerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
